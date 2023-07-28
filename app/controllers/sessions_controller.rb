0f3d5788ca6c3e72d0eaa8810a63bac409b917b4
Javen Wilson <javenb022@gmail.com>
2023-07-28 16:30:36 -0400

    OAuth login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,25 @@
-class SessionsControler < ApplicationController
-  def create 
-    def create
-      auth_hash = request.env['omniauth.auth']
-      user = User.find_by(uid: auth_hash['uid'])
-  
-      if user
-        session[:user_id] = user.id
-      else
-        user = User.create!(
-          name: auth_hash['info']['name'],
-          email: auth_hash['info']['email'],
-          google_id: auth_hash['uid']
-          token = auth_hash['credentials']['token']
-        )
-        session[:user_id] = user.id
-      end
-  
-      redirect_to root_path
+class SessionsController < ApplicationController
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    user = User.find_by(google_id: auth_hash['uid'])
+
+    if user
+      session[:user_id] = user.id
+    else
+      user = User.create!(
+        name: auth_hash['info']['name'],
+        email: auth_hash['info']['email'],
+        google_id: auth_hash['uid']
+      )
+      session[:user_id] = user.id
     end
 
-  def destroy 
-    session[:user_id] = nil 
-    redirect_to root_path 
+    redirect_to root_path
   end
 
-
-  private 
-
-  def auto_hash
-    request.env['omniauth.auth']
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
