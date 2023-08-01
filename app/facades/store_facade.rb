34d15770f5131bba4df7857c2d0d3377af9f265e
Nick Tassinari <ntass7800@gmail.com>
2023-08-01 15:40:22 -0600

    complete store facade

--- app/facades/store_facade.rb
+++ app/facades/store_facade.rb
@@ -1,31 +1,10 @@
 class StoreFacade
-  def initialize(location, radius)
-    @location = format_location(location)
-    @radius = format_radius(radius.to_i)
-  end
 
-  def stores
-    @stores ||= store_data.map do |data|
-      Store.new(data)
+  def self.get_stores(location, radius)
+    location = location.gsub(' ', '%20')
+    radius = (radius.to_i * 1600)
+    StoresService.get_stores(location, radius)[:data].map do |store|
+      Store.new(store)
     end
-  end
-
-
-  private 
-
-  def service 
-    @service ||= StoresService.new
-  end
-
-  def stores_data 
-    @store_data ||= service.get_stores(@location, @radius)[:data]
-  end
-
-  def format_radius(miles)
-    miles * 1600
-  end
-
-  def format_location(location)
-    location.gsub(' ', '%20')
   end
 end
