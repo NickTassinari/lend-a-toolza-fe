10013d5343ab6d5cf976baa54d9583c2fa190431
Nick Tassinari <ntass7800@gmail.com>
2023-08-02 10:45:49 -0600

    edit chat service for proper? endpoint

--- app/services/chat_service.rb
+++ app/services/chat_service.rb
@@ -1,6 +1,6 @@
 class ChatService 
   def self.chat_request(project)
-    response = conn.get("/api/v1/chat_request")
+    response = conn.get("/api/v1/chat_request", { project: project})
     JSON.parse(response.body, symbolize_names: true)
   end
 
@@ -10,4 +10,5 @@ class ChatService
   def self.conn 
     Faraday.new(url: "https://lend-a-toolza-be.onrender.com" )
   end
+
 end
