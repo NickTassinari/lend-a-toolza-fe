0c0b0e6a7965f6ce06c7039e311bc8eaa7e5640d
Nick Tassinari <ntass7800@gmail.com>
2023-08-02 21:00:11 -0600

    add localhost commented out

--- app/services/stores_service.rb
+++ app/services/stores_service.rb
@@ -7,6 +7,7 @@ class StoresService
   private 
 
   def self.conn 
-    Faraday.new(url: "https://lend-a-toolza-be.onrender.com")
+    Faraday.new(url: "https://lend-a-toolza-be.onrender.com" )
+    # Faraday.new(url: "http://localhost:3000")
   end
 end
